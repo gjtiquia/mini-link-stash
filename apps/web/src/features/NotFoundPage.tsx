e93e5d79417f3ed12122bc829fc531b9e46c7d48
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-29 14:54:50 +0800

    feat: not found page styling

--- apps/web/src/features/NotFoundPage.tsx
+++ apps/web/src/features/NotFoundPage.tsx
@@ -1,5 +1,19 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Link } from "@tanstack/react-router";
+
 export function NotFoundPage() {
     return (
-        <p>Mini Link Stash does not have this page!</p>
+        <div className="h-dvh min-h-svh flex flex-col items-center justify-center gap-8">
+            <h1 className="font-bold text-4xl">
+                Page Not Found
+            </h1>
+
+            <Link
+                to="/"
+                className={buttonVariants({ variant: "default", size: "lg", className: "text-lg" })}
+            >
+                Return Home
+            </Link>
+        </div>
     )
 }
