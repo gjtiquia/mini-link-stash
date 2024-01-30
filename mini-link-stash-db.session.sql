2c1edd25997a205fb063d34007ea23c4f7163867
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-30 23:51:15 +0800

    chore: sql commands to quickly test local dev database

---
+++ mini-link-stash-db.session.sql
@@ -0,0 +1,11 @@
+-- @block
+-- query all sessions
+SELECT *
+FROM session;
+--@block
+-- query all users
+SELECT *
+FROM user;
+-- @block
+-- deletes ALL rows in the session table
+DELETE FROM session
