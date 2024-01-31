8a6bc500c2f0481e075a867f0d82c9c90e0a6995
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-31 13:07:03 +0800

    WIP: change dialog defaults to stash link content

--- apps/web/src/features/app/StashLinkDialog.tsx
+++ apps/web/src/features/app/StashLinkDialog.tsx
@@ -15,31 +15,51 @@ export function StashLinkDialog() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button variant="outline">Edit Profile</Button>
+                <Button size={"lg"} className="text-xl px-28 py-6">
+                    Stash Link
+                </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+
+            <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogTitle>Stash Link</DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when you're done.
+                        Stash a new link. Click stash when you're done.
                     </DialogDescription>
                 </DialogHeader>
+
                 <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="link" className="text-right">
+                            Link
+                        </Label>
+                        <Input id="link" className="col-span-3" />
+                    </div>
+
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label htmlFor="name" className="text-right">
                             Name
                         </Label>
-                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                        <Input id="name" className="col-span-3" />
                     </div>
+
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
+                        <Label htmlFor="tags" className="text-right">
+                            Tags
                         </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3" />
+                        <Input id="tags" className="col-span-3" />
+                    </div>
+
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="notes" className="text-right">
+                            Notes
+                        </Label>
+                        <Input id="notes" className="col-span-3" />
                     </div>
                 </div>
+
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button type="submit">Stash</Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
