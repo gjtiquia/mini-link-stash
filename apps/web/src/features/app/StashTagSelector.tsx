93b051a59fdf7649e4038694a548c89354981ee2
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-01 11:00:20 +0800

    feat: added TODOs to the stash tag selector

--- apps/web/src/features/app/StashTagSelector.tsx
+++ apps/web/src/features/app/StashTagSelector.tsx
@@ -1,17 +1,10 @@
 import MultipleSelector, { Option } from "@/components/ui/multiple-selector";
 
 const OPTIONS: Option[] = [
-    { label: 'nextjs', value: 'nextjs' },
-    { label: 'React', value: 'react' },
-    { label: 'Remix', value: 'remix' },
-    { label: 'Vite', value: 'vite' },
-    { label: 'Nuxt', value: 'nuxt' },
-    { label: 'Vue', value: 'vue' },
-    { label: 'Svelte', value: 'svelte' },
-    { label: 'Angular', value: 'angular' },
-    { label: 'Ember', value: 'ember' },
-    { label: 'Gatsby', value: 'gatsby' },
-    { label: 'Astro', value: 'astro' },
+    { label: 'Tag 1', value: 'tag-1' },
+    { label: 'Tag 2', value: 'tag-2' },
+    { label: 'Tag 3', value: 'tag-3' },
+    { label: 'Tag 4', value: 'tag-4' },
 ];
 
 interface StashTagSelectorProps {
@@ -20,6 +13,14 @@ interface StashTagSelectorProps {
 }
 
 export function StashTagSelector(props: StashTagSelectorProps) {
+
+    // TODO : Should search once and get all the tags with react query
+    // TODO : Query before opening the dialog, fetch all the current available tags
+    // TODO : Probably fetch up in dashboard and pass it down as props...? Cuz need to fetch other stuff too anyways
+
+    // TODO : Handle edge case when entering multiple tags of the same name ...
+    // TODO : Probably submit a pull request too if fixed
+
     return (
         <MultipleSelector
             value={props.value}
