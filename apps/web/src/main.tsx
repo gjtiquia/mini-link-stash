93712d5c1d724fced9c967c296e07af205e82f4f
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-01 09:31:27 +0800

    feat: optimization - loading element before first load, fixed initial white screen

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
-import '@/index.css'
+// import '@/index.css' // import stylesheet in html instead so that loading element will be styled before this is called
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
