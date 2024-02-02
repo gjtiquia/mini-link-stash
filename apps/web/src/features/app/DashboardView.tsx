0f27dc026a2a125fb2f3660da8a6d8d683783053
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-02 14:33:00 +0800

    feat: responsive styling for recent links

--- apps/web/src/features/app/DashboardView.tsx
+++ apps/web/src/features/app/DashboardView.tsx
@@ -51,22 +51,62 @@ function RecentLinksView() {
         <ScrollArea className="h-full rounded-md border">
             <ul className="p-2">
                 {getRecentLinksQuery.data?.length > 0 ? getRecentLinksQuery.data?.map((x) => (
-                    <li key={x.link.id} className="break-all">
-                        <div className="
-                            sm:grid sm:auto-cols-fr sm:grid-flow-col
-                        ">
-                            <a href={x.link.url} target="_blank"
-                                className="text-blue-500 hover:text-blue-600 active:text-blue-700"
-                            >
-                                {x.link.url}
-                            </a>
-                            <p>{x.link.modifiedAt}</p>
-                            <div>{x.tags.map((tag) => <p key={tag.id}>{tag.name}</p>)}</div>
-                        </div>
-                        <Separator className="my-2" />
-                    </li>
+                    <RecentLinkElement key={x.link.id} link={{ ...x.link, modifiedAt: new Date(x.link.modifiedAt) }} tags={x.tags} />
                 )) : <p>No links yet</p>}
             </ul>
         </ScrollArea>
+    )
+}
+
+interface RecentLink {
+    id: string,
+    url: string,
+    modifiedAt: Date
+}
+
+interface Tag {
+    id: string,
+    name: string
+}
+
+interface RecentLinkElementProps {
+    link: RecentLink,
+    tags: Tag[]
+}
+
+function RecentLinkElement(props: RecentLinkElementProps) {
+    return (
+        <li key={props.link.id}>
+            <div className="
+                flex flex-col gap-2
+                sm:grid sm:auto-cols-fr sm:grid-flow-col sm:gap-4
+            ">
+                <a href={props.link.url} target="_blank"
+                    className="
+                        text-blue-500 hover:text-blue-600 active:text-blue-700
+                        break-all line-clamp-1
+                        sm:truncate sm:line-clamp-none
+                    "
+                >
+                    {props.link.url}
+                </a>
+
+                <div className="flex gap-4">
+                    <p className="hidden md:block">
+                        {props.link.modifiedAt.toLocaleDateString()}
+                    </p>
+
+                    <div className="flex flex-wrap gap-2">
+                        {/* //TODO: Use shadcn/ui badge instead */}
+                        {props.tags.map((tag) =>
+                            <p key={tag.id} className="whitespace-nowrap bg-green-500 text-secondary text-sm font-bold px-1 rounded-md h-fit">
+                                {tag.name}
+                            </p>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <Separator className="my-2" />
+        </li>
     )
 }
